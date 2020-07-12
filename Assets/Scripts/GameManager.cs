c295af0aca6e2eac65a0bd59ac83463b2e761fd1
Alexis Morel <alexismoreldev@gmail.com>
2020-07-12 13:46:56 -0400

    Singleton implementation of GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager m_instance = null;
+
+    private void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+        }
+        else if (m_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        Debug.Log("test test");
+    }
+}
---
+++ Assets/Scripts/GameManagerLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerLoader : MonoBehaviour
+{
+    public GameManager m_gameManager;
+
+    private void Awake()
+    {
+        if (GameManager.m_instance == null)
+        {
+            Instantiate(m_gameManager);
+        }
+    }
+}
